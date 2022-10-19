05e01ec360d9a591b13dd885d155019dfa1aa994
홍정민(GitMaster) <jm90.hong@gmail.com>
2022-10-20 00:13:15 +0900

    1

--- routes/app.js
+++ routes/app.js
@@ -7,13 +7,20 @@ const app = express()
 const port = 3000
 
 
+//view 엔진을 ejs로 설정
 app.set('view engine', 'ejs');
-app.set('views', '../view/ejs');
-app.set("views", path.join(__dirname, "../view/ejs"));
+
+//ejs 기본 경로 설정
+app.set('views', path.join(__dirname, "../view/ejs"));
+
+//정적(static) 파일 경로 설정
 app.use(express.static(path.join(__dirname, '../public')));
 
 
+//라우터 등록
 app.use('/user', user); //요청시 127.0.0.1:3000/user/path
+
+//파싱
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
--- routes/user.js
+++ routes/user.js
@@ -8,10 +8,12 @@ var router = express.Router();
 router.get('/bird', function(req, res) {
   res.send(`Birds home page ${test}`);
 });
+
 // define the about route
 router.get('/about', function(req, res) {
   res.send('About birds');
 });
 
 
+//외부 파일에서 user를 사용할 수 있게 module export 
 module.exports = router;
